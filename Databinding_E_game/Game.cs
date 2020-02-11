5486809e78b61f02fe208cb97d42515a341149b5
nbourre <nbourrereg@hotmail.com>
2020-02-11 09:46:28 -0500

    Added a example with a game viewer

---
+++ Databinding_E_game/Game.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Databinding_E_game
+{
+    public class Game : INotifyPropertyChanged
+    {
+        private double rating;
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Editor { get; set; }
+        public int Year { get; set; }
+        public string Console { get; set; }
+
+        public double Rating {
+            get => rating;
+            set { 
+                rating = value;
+                OnPropertyChanged();
+            }
+        }
+        public string CoverPath { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Databinding_E_game/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Databinding_E_game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private Game currentGame;
+
+        public List<Game> games { get; set; }
+        private int maxIndex;
+
+        public Game CurrentGame {
+            get => currentGame;
+            set
+            {
+                currentGame = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int MaxIndex { get => maxIndex; set => maxIndex = value; }
+
+        public MainWindow()
+        {
+            games = new List<Game>()
+            {
+                new Game {Title = "Super Mario Bros.",
+                    Console="Switch", CoverPath="images/super_mario.png",
+                    Description="Plumber who's never tired of saving the Princess.",
+                    Editor="Nintendo", Rating=10, Year=1985},
+
+                new Game {Title = "Ark",
+                    Console="XBox", CoverPath="images/ark.jpg",
+                    Description="Survive in a dinosaur world",
+                    Editor="I don't remember", Rating=7, Year=2014},
+
+                new Game {Title = "Balloon Fight",
+                    Console="PS4", CoverPath="images/balloon_fight.jpg",
+                    Description="Ballon nerds fighting",
+                    Editor="Something nintendo", Rating=8, Year=1988},
+
+            };
+
+            CurrentGame = games[0];
+            MaxIndex = games.Count - 1;
+
+            InitializeComponent();
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            CurrentGame.Rating = e.NewValue;
+        }
+
+        private void ScrollBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Debug.WriteLine((int)e.NewValue);
+
+            int newIndex = (int)e.NewValue;
+
+            CurrentGame = games[newIndex];
+
+        }
+    }
+}
